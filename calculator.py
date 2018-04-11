9d95c7b19a40eb20e6a1f8caf0bbc01491661ed2
Untitled <john.d.reddock@gmail.com>
2018-04-11 19:32:02 -0400

    application class - for real

---
+++ calculator.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+class Application(Frame):
+    def __init__(self, master)
+
+    super (Application, self).__init__(master)
+    self.task=""
+    self.UserIn = StringVar()
+    self.grid()
+    self.create_widgets()
+
+    
+
