df68c476832a821681d6b9d3d2465131c707c99c
Untitled <john.d.reddock@gmail.com>
2018-04-14 15:47:55 -0400

    bug fixes, tweaking code

--- weight_conversion.py
+++ weight_conversion.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 def calculate(*args):
     try:
         value = float(feet.get())
-        meters.set((0.3048 * value * 10000.0 + 0.5/10000.0))
+        meters.set((0.3048 * value * 10000.0 + 0.5)/10000.0)
     except ValueError:
         pass
 
@@ -28,5 +28,10 @@ ttk.Button(mainframe, text='Calculate', command=calculate).grid(column=3, row=3,
 ttk.Label(mainframe, text='feet').grid(column=3, row=1, sticky=W)
 ttk.Label(mainframe, text='is equivalent to').grid(column=1, row=2, sticky=W)
 ttk.Label(mainframe, text='meters').grid(column=3, row=2, sticky=W)
+
+for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
+
+feet_entry.focus()
+root.bind('<Return>', calculate)
 
 root.mainloop()
