0e596bb2f675a5cc45e4e6c788b7691b9c0c1166
Untitled <john.d.reddock@gmail.com>
2018-04-16 15:18:22 -0400

    fixed formatting issues

--- mult_generator.py
+++ mult_generator.py
@@ -5,7 +5,7 @@ def TimesTable():
     print('\n')
     for x in range(1, 13):
         m = int(EnterTable.get())
-        print('t', (x), ' x ', (m), '=', (x * m,))
+        print('\t\t', (x), ' x ', (m), ' = ', (x * m),)
 
 Multiply = Tk()
 Multiply.geometry('250x250+700+200')
@@ -15,6 +15,12 @@ EnterTable=StringVar()
 
 label1=Label(Multiply, text='Mult Times Table', font=30, fg='Black').grid(row=1,column=6)
 label1=Label(Multiply, text='                                 ').grid(row=2, column=6)
+entry5=Entry(Multiply, textvariable=EnterTable, justify='center').grid(row=3, column=6)
+label1=Label(Multiply, text='                                 ').grid(row=4, column=6)
+
+button1 = Button(Multiply, text='Times Table', command=TimesTable).grid(row=5, column=6)
+label1=Label(Multiply, text='                                 ').grid(row=6, column=6)
+QUIT = Button(Multiply, text='QUIT', fg='Red', command=Multiply.destroy).grid(row=7, column=6)
 
 Multiply.mainloop()
 
